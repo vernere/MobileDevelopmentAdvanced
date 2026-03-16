b5663b20019be292463bdc58ca1d2af96bbe3232
vernere <verner.etola@metropolia.fi>
2026-03-16 11:39:58 +0200

    First apple dev tutorial

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Verner Etola on 16.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
