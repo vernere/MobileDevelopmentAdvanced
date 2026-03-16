c8833ab9df5276f78ed96c81126ca302132f5dd9
vernere <verner.etola@metropolia.fi>
2026-03-16 11:28:11 +0200

    removed test

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+func arvosana(pisteet p: Double) -> Int? {
+    if p < 0.0 || p > 100 {
+        return nil
+    }
+    if p < 40 {
+        return 0
+    } else {
+        return Int(round(0.5 + (p - 40.0)/12.0))
+    }
+}
+
+arvosana(pisteet: 8.5)
+arvosana(pisteet: 40.0)
+arvosana(pisteet: -5.0)
+arvosana(pisteet: 110.0)
