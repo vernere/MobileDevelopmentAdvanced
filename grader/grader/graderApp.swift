409143b8704fb0932fba4106301abdedb4e9bbe9
vernere <verner.etola@metropolia.fi>
2026-03-16 14:02:19 +0200

    Grader version 1 without calculations

---
+++ grader/grader/graderApp.swift
@@ -0,0 +1,17 @@
+//
+//  graderApp.swift
+//  grader
+//
+//  Created by Verner Etola on 16.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct graderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
