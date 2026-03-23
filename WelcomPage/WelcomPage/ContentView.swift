1f9562825b4cb6e80ae843e1a5b3adee7623050b
Vernere <Verner.etola@metropolia.fi>
2026-03-23 17:04:46 +0200

    Interface tutorial

--- WelcomPage/WelcomPage/ContentView.swift
+++ WelcomPage/WelcomPage/ContentView.swift
@@ -7,15 +7,22 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            WelcomePage()
+            FeaturesPage()
         }
-        .padding()
+        .background(Gradient(colors: gradientColors))
+        #if os(iOS)
+        .tabViewStyle(.page)
+        #endif
+        .foregroundStyle(.white)
     }
 }
 
--- WelcomPage/WelcomPage/FeaturesPage.swift
+++ WelcomPage/WelcomPage/FeaturesPage.swift
@@ -9,10 +9,26 @@ import SwiftUI
 
 struct FeaturesPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+                .padding(.top, 100)
+            
+            FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+            
+            FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
 #Preview {
     FeaturesPage()
+        .frame(maxHeight: .infinity)
+        .background(Gradient(colors: gradientColors))
+        .foregroundStyle(.white)
 }
