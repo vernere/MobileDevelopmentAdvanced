bb67f16be8ff3b3f26e24a831ce2587a17ade63c
Vernere <Verner.etola@metropolia.fi>
2026-03-23 14:39:43 +0200

    Grader completed

--- Grader.playground/Contents.swift
+++ Grader.playground/Contents.swift
@@ -3,37 +3,57 @@ import UIKit
 
 
 func grader(percentages: (Int, Int, Int)) -> ((Double, Double, Double) -> Int?)? {
-    let (p1, p2, p3) = percentages
+    let (w1, w2, w3) = percentages
     
-    if p1 < 0 ||  p2 < 0  || p3 < 0 || p1 + p2 + p3 != 100  {
+    if w1 < 0 ||  w2 < 0  || w3 < 0 || w1 + w2 + w3 != 100  {
         return nil
     }
         
-    return { (p1: Double, p2: Double, p3: Double) -> Int? in
+    return { (points1: Double, points2: Double, points3: Double) -> Int? in
         
-        if p1 < 0 || p1 > 100 || p2 < 0 || p2 > 100 || p3 < 0 || p3 > 100  {
+        if points1 < 0 || points1 > 100 ||
+            points2 < 0 || points2 > 100 ||
+            points3 < 0 || points3 > 100  {
             return nil
         }
         
-        let average = p1 * Double(p1)/100 + p2 * Double(p2)/100 + p3 * Double(p3)/100
+        let total = points1 * Double(w1) / 100.0 + points2 * Double(w2) / 100.0 + points3 * Double(w3) / 100.0
         
-        if average >= 90 {
-            return 5
-        } else if average >= 80 {
-            return 4
-        } else if average >= 70 {
-            return 3
-        } else if average >= 60 {
-            return 2
-        } else if average >= 50 {
-            return 1
-        } else {
+        if total < 40 {
             return 0
         }
+        
+        let result = 0.5 + (total - 40.0) * (4.99 / 60.0)
+        
+        return Int(result.rounded())
     }
 }
 
+let g3 = grader(percentages: (61, 30, 10))
+if let g3 = g3 {
+    print(g3(40.0, 40.0, 40.0) ?? "Error")
+} else {
+    print("grader missing")
+}
 
-if let g = grader(percentages: (30, 30, 40)) {
-    print(g(80, 90, 85)!)
+let g1 = grader(percentages: (60, 30, 10))
+if let g1 = g1 {
+    print(g1(40.0, 40.0, 40.0) ?? "Error")
+    print(g1(100.0, 100.0, 100.0) ?? "Error")
+    print(g1(100.0, 11.0, 0.0) ?? "Error")
+}
+
+let g2 = grader(percentages: (80, 20, 0))
+if let g2 = g2 {
+    print(g2(40.0, 40.0, 40.0) ?? "Error")
+    print(g2(100.0, 100.0, 100.0) ?? "Error")
+    print(g2(100.0, 110.0, 0.0) ?? "Error")
+}
+
+print("Min total points for each grade")
+for grade in 1...5{
+    let minIntermediate = Double(grade) - 0.5
+    let minTotal = 40.0 + (minIntermediate - 0.5) * (60.0 / 4.99)
+    let result = max(minTotal, 40.0)
+    print("Grade \(grade): \(String(format: "%.2f", result)) total points")
 }
