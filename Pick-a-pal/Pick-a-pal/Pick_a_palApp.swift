91ab9ba5e1ed4ad53601a0ca3fe333b9c4c5af09
Vernere <Verner.etola@metropolia.fi>
2026-03-28 16:16:05 +0200

    Pick-a-pal completed

---
+++ Pick-a-pal/Pick-a-pal/Pick_a_palApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_palApp.swift
+//  Pick-a-pal
+//
+//  Created by Vertti on 28.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_palApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
