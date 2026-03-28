7df015a5b21d0088aaaed541b77ee3d8bfe13c1e
Vernere <Verner.etola@metropolia.fi>
2026-03-28 15:48:35 +0200

    DiceRoller completed

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Vertti on 28.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
